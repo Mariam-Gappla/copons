82743637eb7932a0d35901a0ec2c5426727078c0
mariam-Gaballah <mariam@smartsols.net>
2025-11-29 14:05:21 +0200

    fix error on upload image

--- configration/saveImage.js
+++ configration/saveImage.js
@@ -1,17 +1,23 @@
 const path = require("path");
 const fs = require("fs");
 
-const saveImage = (file, folder = 'images') => {
-    const fileName = `${Date.now()}-${file.originalname}`;
-    const saveDir = path.join(__dirname, '..', folder);
-    const filePath = path.join(saveDir, fileName);
+const saveImage = (file, folder = '/var/www/images') => {
+  // نشيل المسافات ونحوّلها لشرطة سفلية أو نشيلها خالص
+  const safeName = file.originalname.replace(/\s+/g, "_");
+  const fileName = `${Date.now()}-${safeName}`;
+  const saveDir = folder; // المسار المطلق
+  const filePath = path.join(saveDir, fileName);
 
-    if (!fs.existsSync(saveDir)) {
-        fs.mkdirSync(saveDir, { recursive: true });
-    }
+  if (!fs.existsSync(saveDir)) {
+    fs.mkdirSync(saveDir, { recursive: true });
+  }
 
-    fs.writeFileSync(filePath, file.buffer);
-    return `/${folder}/${fileName}`;
+  fs.writeFileSync(filePath, file.buffer);
+
+  console.log("Saved file at:", filePath);
+
+  // الرابط اللي هيتخزن في الداتابيز
+  return `images/${fileName}`;
 };
 
 module.exports = saveImage;
